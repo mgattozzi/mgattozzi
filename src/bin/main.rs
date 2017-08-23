5b72c19e1b59ace085ee9e62501be86857b22b23
Michael Gattozzi <mgattozzi@gmail.com>
2017-08-22 20:39:52 -0400

    Refactor Site to be more ergonomic
    This does a few things:
    
    1) Rips out React.js
    2) Uses Terra Templates via Rocket
    3) Makes it so that posts put in posts become a page in the routing
    4) Maintains exact same site logic
    
    This won't affect SEO or change current links but keeps the site as is
    while being easier for me to work with.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,37 +1,39 @@
 #![feature(plugin, custom_derive, custom_attribute)]
 #![plugin(rocket_codegen)]
 
-#[macro_use]
-extern crate lazy_static;
+#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate lazy_static;
+extern crate comrak;
+extern crate diesel;
 extern crate rocket;
 extern crate rocket_contrib;
-extern crate diesel;
-extern crate mlib;
-#[macro_use]
-extern crate serde_derive;
-extern crate serde_json;
 extern crate r2d2;
 extern crate r2d2_diesel;
+extern crate mlib;
+extern crate serde_json;
+extern crate tera;
 
+use comrak::{ markdown_to_html, ComrakOptions };
 // Server Imports
 // Used to Setup DB Pool
-use rocket::request::{Outcome, FromRequest};
-use rocket::Outcome::{Success, Failure};
+use rocket::request::{ Outcome, FromRequest };
+use rocket::Outcome::{ Success, Failure };
 use rocket::http::Status;
 
 // Used for Routes
 use rocket::Request;
 use rocket::response::NamedFile;
-use rocket_contrib::JSON;
+use rocket_contrib::{ Template, Json };
 
 // Std Imports
-use std::path::{Path, PathBuf};
+use std::path::{ Path, PathBuf };
+use std::io::{ BufReader, Read };
 
 // DB Imports
 use diesel::prelude::*;
 use diesel::update;
 use diesel::pg::PgConnection;
-use r2d2::{Pool, PooledConnection, GetTimeout};
+use r2d2::{ Pool, PooledConnection, GetTimeout };
 use r2d2_diesel::ConnectionManager;
 use mlib::models::*;
 use mlib::*;
@@ -43,12 +45,12 @@ fn main() {
         .mount("/", routes![
             count,
             count_update,
-            public,
             keybase,
             static_files,
             index,
-            site
+            posts
         ])
+        .attach(Template::fairing())
         .launch();
 }
 
@@ -76,45 +78,68 @@ impl<'a, 'r> FromRequest<'a, 'r> for DB {
 }
 
 // Routes
-#[get("/static/<file..>")]
+#[get("/static/<file..>", rank=1)]
 fn static_files(file: PathBuf) -> Option<NamedFile>{
-     NamedFile::open(Path::new("src/client/static").join(file)).ok()
+    NamedFile::open(Path::new("static").join(file)).ok()
 }
 
-#[get("/public/<file..>")]
-fn public(file: PathBuf) -> Option<NamedFile>{
-     NamedFile::open(Path::new("src/client/public").join(file)).ok()
+#[get("/<posts..>", rank=2)]
+fn posts(mut posts: PathBuf) -> Option<Template> {
+    posts.set_extension("md");
+    let path = Path::new("posts").join(&posts);
+
+    let mut buffer = String::new();
+    let file = NamedFile::open(&path);
+
+    match file {
+        Ok(file) => {
+            let mut reader = BufReader::new(file);
+            match reader.read_to_string(&mut buffer) {
+                Ok(_) => {
+                    let post = Post::new(markdown_to_html(&buffer, &ComrakOptions::default()));
+                    Some(Template::render("post", post))
+                },
+                Err(_) => None
+            }
+        },
+        Err(_) => {
+            posts.set_extension("");
+            let nav = posts.to_str().unwrap().clone();
+            match nav {
+                "about"    => Some(Template::render("about",    Nav::new(1))),
+                "archive"  => Some(Template::render("archive",  Nav::new(2))),
+                "contact"  => Some(Template::render("contact",  Nav::new(3))),
+                "resume"   => Some(Template::render("resume",   Nav::new(4))),
+                "counting" => Some(Template::render("counting", Nav::new(5))),
+                _          => None,
+            }
+        },
+    }
 }
 
 #[get("/keybase.txt")]
 fn keybase() -> Option<NamedFile> {
-    NamedFile::open("src/client/keybase.txt").ok()
+    NamedFile::open("keybase.txt").ok()
 }
 
 #[get("/")]
-fn index() -> Option<NamedFile> {
-    NamedFile::open("src/client/index.html").ok()
-}
-
-#[get("/<path..>")]
-fn site(path: PathBuf) -> Option<NamedFile> {
-    let _ = path;
-    NamedFile::open("src/client/index.html").ok()
+fn index() -> Option<Template> {
+    Some(Template::render("index", Nav::new(0)))
 }
 
 #[get("/count")]
-fn count(db: DB) -> JSON<Clicks> {
+fn count(db: DB) -> Json<Clicks> {
     use mlib::schema::counts::dsl::*;
     let result = counts.first::<Count>(db.conn())
         .expect("Error loading clicks");
 
-    JSON(Clicks {
+    Json(Clicks {
         count: result.clicks,
     })
 }
 
 #[put("/count")]
-fn count_update(db: DB) -> JSON<Clicks> {
+fn count_update(db: DB) -> Json<Clicks> {
     use mlib::schema::counts::dsl::*;
     let query = counts.first::<Count>(db.conn())
         .expect("Error loading clicks");
@@ -125,12 +150,35 @@ fn count_update(db: DB) -> JSON<Clicks> {
         .execute(db.conn())
         .unwrap();
 
-    JSON(Clicks {
+    Json(Clicks {
         count: val,
     })
 }
 
 #[derive(Deserialize, Serialize)]
+pub struct Nav {
+    url: i32,
+}
+
+impl Nav {
+    pub fn new(url: i32) -> Self {
+        Self { url }
+    }
+}
+
+#[derive(Deserialize, Serialize)]
 pub struct Clicks {
     pub count: i32,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Post {
+    data: String,
+    url: i32,
+}
+
+impl Post {
+    pub fn new(data: String) -> Self {
+        Self { data, url: -1 }
+    }
 }
